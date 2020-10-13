7c4e1f073fd6b63006b84554542ef3d1e573853e
rangademetal <stro3andr3i@gmail.com>
2020-10-13 15:51:03 -0700

    Update

--- Test/AT1.py
+++ Test/AT1.py
@@ -1,21 +1,26 @@
 import unittest
-
+import os
+from pathlib import Path
 from Database.Secret import DB_DATABASE, DB_HOST, DB_USERNAME, DB_PASSWORD
 
-
+# Test database parameters and
 class InputTest(unittest.TestCase):
     def setUp(self):
         pass
 
-    # Database connection
+    # Check enviroment parameters with database connection
     def test_database(self):
         self.assertEqual(DB_USERNAME, 'aliexpress')
         self.assertEqual(DB_HOST, 'localhost')
         self.assertEqual(DB_PASSWORD, 'aliexpress')
         self.assertEqual(DB_DATABASE, 'aliexpress')
-
+    # Check webdriver is located in project file
     def test_location(self):
-        pass
+        path = os.getcwd()
+        path = Path(path)
+        os.chdir(path.parent)
+        self.assertEqual(True, os.path.exists('chromedriver.exe'), 'Put your chromrdriver.exe in AliExpress folder !!!')
+
 
 if __name__ == '__main__':
     unittest.main()
