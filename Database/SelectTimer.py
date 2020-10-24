9320128ba08edbeb68932f3d94a9b5f00b358ac7
rangademetal <stro3andr3i@gmail.com>
2020-10-24 08:24:54 -0700

    Update

---
+++ Database/SelectTimer.py
@@ -0,0 +1,19 @@
+from Database.Secret import *
+from Database.ConnectionDatabase import ConnectionDatabase
+from prettytable import PrettyTable
+
+database = ConnectionDatabase(DB_DATABASE)
+db = database.connect_database()
+table = PrettyTable()
+
+try:
+    cursor = db.cursor()
+    sql = "SELECT source, timer FROM timer WHERE timer > 1.500"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    table.field_names = ['Source', 'Time']
+    for i in result:
+        table.add_row([i[0], i[1]])
+    print(table)
+except Exception as e:
+    print(f'ERROR - {e}')
--- Emag/Insert.py
+++ Emag/Insert.py
@@ -2,16 +2,21 @@ from Emag.Category import Category
 from Emag.PageTimer import Timer
 import threading
 
-# category = Category()
-# category.get_category('https://www.emag.ro/desktop-pc/c?ref=hp_menu_quick-nav_23_1&type=category')
-# category.get_category('https://www.emag.ro/laptopuri/c')
-# del category
+try:
+    category = Category()
+    category.get_category('https://www.emag.ro/desktop-pc/c?ref=hp_menu_quick-nav_23_1&type=category')
+    category.get_category('https://www.emag.ro/laptopuri/c')
+    category.get_category('https://www.emag.ro/placi_video/c?ref=effective_search')
+    category.get_category('https://www.emag.ro/memorii/c?ref=effective_search')
+    del category
 
-timer = Timer()
-th1 = threading.Thread(target=timer.timer_, args=('https://www.emag.ro/laptopuri/c?ref=effective_search', 26, 'https://www.emag.ro/laptopuri',))
-th2 = threading.Thread(target=timer.timer_, args=('https://www.emag.ro/desktop-pc/c?ref=effective_search', 246, 'https://www.emag.ro/desktop-pc',))
+    timer = Timer()
 
-th1.start()
-th2.start()
+    timer.timer_('https://www.emag.ro/laptopuri/c?ref=effective_search', 26, 'https://www.emag.ro/laptopuri')
+    timer.timer_('https://www.emag.ro/desktop-pc/c?ref=effective_search', 246, 'https://www.emag.ro/desktop-pc')
+    timer.timer_('https://www.emag.ro/placi_video/c?ref=effective_search', 12, 'https://www.emag.ro/placi_video')
+    timer.timer_('https://www.emag.ro/memorii/c?ref=effective_search', 28, 'https://www.emag.ro/memorii')
 
-del timer
+    del timer
+except Exception as e:
+    print(f'ERROR - {e}')
