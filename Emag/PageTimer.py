899eb5287cac224028284c96b1ae51daecaf37bb
rangademetal <stro3andr3i@gmail.com>
2020-10-27 02:27:38 -0700

    Update

--- Emag/PageTimer.py
+++ Emag/PageTimer.py
@@ -1,7 +1,5 @@
 from Emag.WebDriver import Initialization
-from Database.ConnectionDatabase import ConnectionDatabase
-from Database.Secret import *
-from mysql.connector.errors import Error
+from Database.QueryDatabase import Query
 from selenium.common.exceptions import NoSuchElementException
 import time
 
@@ -9,54 +7,46 @@ import time
 class Timer:
     def __init__(self, ):
         self.scanner = Initialization()
-        self.database = ConnectionDatabase(DB_DATABASE)
+        self.database = Query()
 
     def timer_(self, source, pages, url):
-
-        db = self.database.connect_database()
         self.scanner.driver.get(source)
         category = self.scanner.driver.find_element_by_class_name('title-phrasing-xl')
-        category = category.text
-        print(category)
-        cursor = db.cursor(buffered=True)
-        sql = 'SELECT id from category where name_category = %s'
-        val = (category, )
-        cursor.execute(sql, val)
-        res = cursor.fetchone()
-        id_cat = res
+        id_category = self.database.select_id(category.text)
 
         for i in range(1, pages):
-
+            count = 1
+            scroll = 0
             start = time.perf_counter()
             self.scanner.driver.get(f'{url}/p{i}/c')
-            for j in range(1, 62):
+            while count != 62:
+                self.scanner.driver.execute_script('window.scrollTo(0, ' + str(scroll) + ')')
+                print(count)
                 try:
-                    start = time.perf_counter()
-                    clicker = self.scanner.driver.find_element_by_xpath(f'//*[@id="card_grid"]/div[{i}]/div/div/div[2]/h2/a')
-                    clicker.click()
-                    stop = time.perf_counter()
-                    timer = f'{stop - start:0.4f}'
-                    timer = float(timer)
-                    print(timer)
-                    self.scanner.driver.execute_script("window.history.go(-1)")
-                except Error as e:
-                    print(f'ERROR - {e}')
+                    clickers = self.scanner.driver.find_element_by_xpath(
+                        f'//*[@id="card_grid"]/div[{count}]/div/div/div[2]/h2/a')
+                except NoSuchElementException as e:
+                    continue
+
+                start = time.perf_counter()
+                clickers.click()
+                stop = time.perf_counter()
+                count += 1
+                if count % 5 == 0:
+                    count += 1
+
+                scroll += 145
+                timer = f'{stop - start:0.4f}'
+                timer = float(timer)
+                print(timer)
+                self.scanner.driver.execute_script("window.history.go(-1)")
+                time.sleep(1)
 
             stop = time.perf_counter()
             timer = f'{stop - start:0.4f}'
             timer = float(timer)
             print(f'Page {self.scanner.driver.current_url} have {stop - start:0.4f} seconds')
-            try:
-                sql = 'INSERT INTO timer(timer, id_category, source) values(%s, %s, %s)'
-                val = (timer, id_cat[0], self.scanner.driver.current_url)
-                cursor.execute(sql, val)
-                db.commit()
-            except Error as e:
-                print('Update in Database')
-                sql = 'UPDATE timer SET timer = %s WHERE source = %s'
-                val = (timer, self.scanner.driver.current_url)
-                cursor.execute(sql, val)
-                db.commit()
+            self.database.insert_update(timer, id_category, self.scanner.driver.current_url)
 
     def __del__(self):
         self.scanner.driver.quit()
